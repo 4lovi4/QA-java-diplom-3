4859d6881bdd4df93623b65b514857f9f924c7d7
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2022-09-13 01:25:27 +0300

    tests update

--- src/main/java/page_object/RegisterPage.java
+++ src/main/java/page_object/RegisterPage.java
@@ -6,10 +6,32 @@ import org.openqa.selenium.support.How;
 
 public class RegisterPage {
 
-    @FindBy(how = How.XPATH, using = ".//a[@class='AppHeader_header__linkText__3q_va ml-2']")
+    public static final String Path = "register";
+
+    @FindBy(how = How.XPATH, using = ".//p[text()='Конструктор']")
     private SelenideElement constructorButton;
 
-    @FindBy(how = How.XPATH, using = ".//a[@class='AppHeader_header__linkText__3q_va ml-2']")
+    @FindBy(how = How.XPATH, using = ".//p[text()='Лента Заказов']")
     private SelenideElement ordersFeed;
 
+    @FindBy(how = How.XPATH, using = ".//p[text()='Личный Кабинет']")
+    private SelenideElement profile;
+
+    @FindBy(how = How.XPATH, using = ".//div[@class='AppHeader_header__logo__2D0X2']")
+    private SelenideElement stellarLogo;
+
+    @FindBy(how = How.XPATH, using = ".//label[text()='Имя']")
+    private SelenideElement nameInput;
+
+    @FindBy(how = How.XPATH, using = ".//label[text()='Email']")
+    private SelenideElement emailInput;
+
+    @FindBy(how = How.XPATH, using = ".//label[text()='Пароль']")
+    private SelenideElement passwordInput;
+
+    @FindBy(how = How.XPATH, using = ".//div[@class='input__icon input__icon-action']")
+    private SelenideElement iconEye;
+
+    @FindBy(how = How.XPATH, using = ".//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']")
+    private SelenideElement registerButton;
 }
